1430b39c2be6843e226b1e1e8b1cadb514f2aa02
cxx <395413654@qq.com>
2018-03-16 13:17:03 +0800

    test

---
+++ runmpc.sh
@@ -0,0 +1,15 @@
+
+cd ./DataModule/Linux_Bin
+./runlinuxmpc.sh
+
+cd ../../UnitTest/Linux_bin
+./runlinuxmpc.sh
+
+cd ../../BusinessModule/Linux_bin
+./runlinuxmpc.sh
+
+cd ../../PurenessScopeServer/Linux_Bin
+./runlinuxmpc.sh
+
+cd ../../
+
